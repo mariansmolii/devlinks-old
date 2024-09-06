d651ebaa39fe6a33c97736bdf72e3c9b2b4a4db0
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-09-06 20:19:50 +0300

    create auth schemes

---
+++ client/src/utils/schemas/validationSchemas.js
@@ -0,0 +1,31 @@
+import * as Yup from "yup";
+
+const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+
+const loginValidationSchema = Yup.object({
+  email: Yup.string()
+    .matches(emailRegex, "Invalid email address")
+    .required("Can't be empty"),
+  password: Yup.string()
+    .min(8, "At least 8 characters")
+    .required("Can't be empty"),
+});
+
+export const registerValidationSchema = Yup.object({
+  email: Yup.string()
+    .matches(emailRegex, "Invalid email address")
+    .required("Can't be empty"),
+  password: Yup.string()
+    .min(8, "At least 8 characters")
+    .required("Can't be empty"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Passwords must match.")
+    .required("Can't be empty"),
+});
+
+const authSchemes = {
+  loginValidationSchema,
+  registerValidationSchema,
+};
+
+export default authSchemes;
