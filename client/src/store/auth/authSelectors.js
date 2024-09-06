5c6939bdf7356155322f62c7ad5780c61c99826d
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-09-06 20:11:36 +0300

    create auth selectors

---
+++ client/src/store/auth/authSelectors.js
@@ -0,0 +1,11 @@
+export const selectToken = (state) => state.auth.token;
+
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
+
+export const selectIsLoading = (state) => state.auth.isLoading;
+
+export const selectError = (state) => state.auth.error;
+
+export const selectUserEmail = (state) => state.auth.user.email;
+
+export const selectIsRefreshing = (state) => state.auth.isRefreshing;
