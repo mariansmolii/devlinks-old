83fda98a602edf62ad36f5752804cb4239f99f01
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2024-09-09 18:32:52 +0300

    create profile page

--- client/src/pages/ProfilePage/ProfilePage.jsx
+++ client/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,28 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getProfile } from "../../store/profile/profileOperations";
+
+import PageTitle from "../../components/PageTitle/PageTitle";
+import Section from "../../components/Section/Section";
+import ProfileForm from "../../components/ProfileForm/ProfileForm";
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProfile());
+  }, [dispatch]);
+
+  return (
+    <Section>
+      <PageTitle
+        title="Profile Details"
+        subtitle="Add your details to create a personal touch to your profile."
+      />
+
+      <ProfileForm />
+    </Section>
+  );
 };
 
 export default ProfilePage;
