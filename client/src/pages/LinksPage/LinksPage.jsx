0d2b6572c5c72d068826f0991c332c39873b8503
Maryan Smoliy <smoliymaryan89@gmail.com>
2024-09-13 19:13:18 +0300

    refactor code and add toast

--- client/src/pages/LinksPage/LinksPage.jsx
+++ client/src/pages/LinksPage/LinksPage.jsx
@@ -1,33 +1,28 @@
 import { nanoid } from "nanoid";
 import { useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
-import {
-  createLink,
-  getAllLinks,
-  updateLinks,
-} from "../../store/links/linksOperations";
+import { createLink, updateLinks } from "../../store/links/linksOperations";
 import { addLink, getLinkData } from "../../store/links/linksSlice";
 
-import Button from "../../components/Button/Button";
-import LinksList from "../../components/LinksList/LinksList";
-import PageTitle from "../../components/PageTitle/PageTitle";
-import Section from "../../components/Section/Section";
-import styles from "./LinksPage.module.scss";
-import LinkInstruction from "../../components/LinkInstruction/LinkInstruction";
+import toast from "react-hot-toast";
 import useLinks from "../../hooks/useLinks";
+import Button from "../../components/Button/Button";
+import Section from "../../components/Section/Section";
+import PageTitle from "../../components/PageTitle/PageTitle";
+import LinksList from "../../components/LinksList/LinksList";
+import CustomToast from "../../components/CustomToast/CustomToast";
+import LinkInstruction from "../../components/LinkInstruction/LinkInstruction";
 import options from "../../utils/data/selectData";
+import styles from "./LinksPage.module.scss";
 
 const LinksPage = () => {
-  const [allLinks, setAllLinks] = useState([]);
-  const [selectedPlatform, setSelectedPlatform] = useState(options[0]);
-  const [errors, setErrors] = useState({});
-
   const dispatch = useDispatch();
   const { links } = useLinks();
 
-  useEffect(() => {
-    dispatch(getAllLinks());
-  }, [dispatch]);
+  const [allLinks, setAllLinks] = useState([]);
+  const [selectedPlatform, setSelectedPlatform] = useState(options[0]);
+  const [errors, setErrors] = useState({});
+  const [inputValues, setInputValues] = useState({});
 
   useEffect(() => {
     const sortedLinks = [...links].sort((a, b) => a.index - b.index);
@@ -48,18 +43,39 @@ const LinksPage = () => {
   };
 
   const hasErrors = Object.values(errors).some((error) => error.isError);
+  const isEmptyValues = Object.values(inputValues).some(
+    (value) => value === ""
+  );
 
-  const handleSave = () => {
-    const isNewLink = allLinks.some((link) => link.type === "new");
+  const handleSave = async () => {
     const noNewLink = allLinks.filter((link) => link.type !== "new");
+    const isNewLink = allLinks.some((link) => link.type === "new");
 
-    if (hasErrors) return;
+    if (hasErrors || isEmptyValues) return;
 
-    if (isNewLink) {
-      dispatch(createLink(allLinks));
+    try {
+      if (isNewLink) {
+        await dispatch(createLink(allLinks));
+      }
+
+      await dispatch(updateLinks(noNewLink));
+
+      toast.custom((t) => (
+        <CustomToast
+          t={t}
+          text="Your changes have been successfully saved!"
+          icon="icon-changes-saved"
+        />
+      ));
+    } catch {
+      toast.custom((t) => (
+        <CustomToast
+          t={t}
+          icon="warning"
+          text="Something went wrong, please try again later!"
+        />
+      ));
     }
-
-    dispatch(updateLinks(noNewLink));
   };
 
   return (
@@ -85,6 +101,9 @@ const LinksPage = () => {
             setSelectedPlatform={setSelectedPlatform}
             errors={errors}
             setErrors={setErrors}
+            inputValues={inputValues}
+            setInputValues={setInputValues}
+            isEmptyValues={isEmptyValues}
           />
         </div>
       ) : (
@@ -96,7 +115,7 @@ const LinksPage = () => {
           type={"button"}
           title={"Save"}
           variant={"primary"}
-          disabled={allLinks.length === 0 || hasErrors}
+          disabled={allLinks.length === 0 || hasErrors || isEmptyValues}
           onClick={handleSave}
         />
       </div>
